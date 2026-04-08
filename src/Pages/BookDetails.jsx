8e7b73e979bea6fcf6da7e928d3e71e825d70eaf
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-08 14:47:05 +0600

    working on book details

--- src/Pages/BookDetails.jsx
+++ src/Pages/BookDetails.jsx
@@ -13,10 +13,31 @@ const BookDetails = () => {
     return (
         <div className='w-4/5 mx-auto flex gap-12 mt-14'>
             <div className='bg-[#F3F3F3] flex items-center justify-center p-10 rounded-2xl'>
-                <img src={ebook.image} className='rotate-x-15 rotate-y-30 hover:-rotate-y-30 duration-700 h-100' alt="" />
+                <img src={ebook.image} className='rotate-x-15 rotate-y-30 hover:-rotate-y-30 duration-700 max-h-screen' alt="" />
             </div>
-            <div>
-
+            <div className='w-full'>
+                <div className='space-y-4'>
+                    <h1 className='text-4xl font-bold! playfair'>{ebook.bookName}</h1>
+                    <p className='font-medium! text-[#131313CC]'>By: {ebook.author}</p>
+                </div>
+                <div className="divider my-1"></div>
+                <div>
+                    <p className='font-semibold! text-[#131313CC]'>{ebook.category}</p>
+                </div>
+                <div className="divider my-1"></div>
+                <div className='space-y-4'>
+                    <p><span className='font-bold'>Review:</span> {ebook.review}</p>
+                    <div className='space-x-2 mb-4'> <span className='font-bold'>Tags:</span>
+                        <span className='work font-medium! text-[#23BE0A] bg-[#23BE0A0D] px-4 py-2 rounded-2xl'>{ebook.tags[0]}</span>
+                        <span className='work font-medium! text-[#23BE0A] bg-[#23BE0A0D] px-4 py-2 rounded-2xl'>{ebook.tags[1]}</span>
+                    </div>
+                </div>
+                <div>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
             </div>
         </div>
     );
